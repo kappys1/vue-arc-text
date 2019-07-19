b26162e01e373a96332a311e113b98c6f4d3ea85
alex marcos <alexsbd1@gmail.com>
2019-07-19 12:58:12 +0200

    ci(dependencies): solve dependencies

--- tools/gh-pages-publish.sh
+++ tools/gh-pages-publish.sh
@@ -2,8 +2,6 @@
 # See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
 set -o errexit
 
-
-
 # build (CHANGE THIS)
 
 # deploy
@@ -16,4 +14,4 @@ git config --global user.email "alexsbd1@gmail.com"
 git config --global user.name "alex marcos"
 
 git commit -m "docs(docs): Deploy to Github Pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/kappys1/react-arc-text.git" master:gh-pages
+git push --force --quiet "https://${GH_TOKEN}@github.com/kappys1/arc-arc-text.git" master:gh-pages
