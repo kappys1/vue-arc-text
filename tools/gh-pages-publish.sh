bfe80baa085b3ac22e340b50e5631d3b6cf5bc53
alex marcos <alexsbd1@gmail.com>
2019-07-19 13:03:57 +0200

    ci(travis): improve to build in travis

--- tools/gh-pages-publish.sh
+++ tools/gh-pages-publish.sh
@@ -14,4 +14,4 @@ git config --global user.email "alexsbd1@gmail.com"
 git config --global user.name "alex marcos"
 
 git commit -m "docs(docs): Deploy to Github Pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/kappys1/arc-arc-text.git" master:gh-pages
+git push --force --quiet "https://${GH_TOKEN}@github.com/kappys1/vue-arc-text.git" master:gh-pages
